1ca11bd41cdd5d7ba31cc2aab0153c5c1784ffab
Samuel Ruby <sam@Samuels-MacBook-Pro.local>
2017-03-11 15:40:16 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
- console.log("Up and running!");
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardTwo)
-console.log("User flipped " + cardThree)
+/*This is codefrom section 7*/
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay)
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay)
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match");
+	}else {
+	   alert("Sorry try again");
+	}
+	
+}
