adf1606d6c0036106508845df167f63ba5fd81e0
Samuel Ruby <sam@Samuels-MacBook-Pro.local>
2017-03-12 06:03:08 -0400

    moved game logice into functions

--- js/main.js
+++ js/main.js
@@ -2,24 +2,23 @@
 var cards = ['queen','queen','king','king'];
 var cardsInPlay = [];
 
-/*This is codefrom section 7*/
-var cardOne = cards[0];
-var cardTwo = cards[1];
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
 
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay)
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay)
-
-if (cardsInPlay.length === 2) {
-
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match");
-	}else {
-	   alert("Sorry try again");
+			if (cardsInPlay[0] === cardsInPlay[1]) {
+				alert("You found a match");
+			}else {
+			   alert("Sorry try again");
+			}	
 	}
-	
 }
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
